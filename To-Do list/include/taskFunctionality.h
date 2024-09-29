d42afa5750f2b947e9dbf3ea96f2ee619bea9b62
RacinskasGiedrius <giedrius.racinskas@stud.viko.lt>
2024-09-30 01:09:52 +0300

    ADD - Functions to edit and delete tasks, updated Headers for better UX and a lot of error handling for different scenarios.

--- To-Do list/include/taskFunctionality.h
+++ To-Do list/include/taskFunctionality.h
@@ -2,7 +2,13 @@
 #define TASKFUNCTIONALITY_H
 
 void mainMenu();
+void addTaskHeader();
+void viewTasksHeader();
+void editTaskHeader();
+void deleteTaskHeader();
 void addTask();
 void viewTasks();
+void editTask();
+void run();
 
 #endif //TASKFUNCTIONALITY_H
--- To-Do list/src/taskFunctionality.cpp
+++ To-Do list/src/taskFunctionality.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <limits>
 
 using namespace std;
 
@@ -16,34 +17,178 @@ void mainMenu() {
     cout << "3. Edit task\n";
     cout << "4. Delete task\n";
     cout << "5. Exit\n";
+    cout << "\n####################################################\n";
+}
+
+void addTaskHeader() {
+    cout << "\n####################################################\n";
+    cout << "#                    TO-DO List                    #\n";
     cout << "####################################################\n";
+    cout << "\n1. Add task\n";
+    cout << "\n####################################################\n";
+}
+
+void viewTasksHeader() {
+    cout << "\n####################################################\n";
+    cout << "#                    TO-DO List                    #\n";
+    cout << "####################################################\n";
+    cout << "\n2. View tasks\n";
+    cout << "\n####################################################\n";
+}
+
+void editTasksHeader() {
+    cout << "\n####################################################\n";
+    cout << "#                    TO-DO List                    #\n";
+    cout << "####################################################\n";
+    cout << "\n3. Edit task\n";
+    cout << "\n####################################################\n";
+}
+
+void deleteTaskHeader() {
+    cout << "\n####################################################\n";
+    cout << "#                    TO-DO List                    #\n";
+    cout << "####################################################\n";
+    cout << "\n4. Delete task\n";
+    cout << "\n####################################################\n";
 }
 
 void addTask() {
     string newTask;
-    string addTaskAction;
+    string addAnotherTask;
 
-    while (addTaskAction != "n") {
+    do {
         cout << "\nEnter task name: \n";
         cin >> newTask;
 
         tasks.push_back(newTask);
-
         cout << "\nTask " << newTask << " added successfully!\n";
-        cout << "\nDo you want to add another task?: \n";
-        cin >> addTaskAction;
-    }
+
+        do {
+            cout << "\nDo you want to add another task? (y/n): \n";
+            cin >> addAnotherTask;
+
+            if (addAnotherTask != "y" && addAnotherTask != "n") {
+                cout << "\nWrong input. Please try again!\n";
+            }
+        } while (addAnotherTask != "y" && addAnotherTask != "n");
+    } while (addAnotherTask == "y");
 }
 
 void viewTasks() {
     int listNumber = 1;
 
-    if (tasks.size() == 0) {
+    if (tasks.empty()) {
         cout << "\nNo tasks found\n";
-    }
-    else {
+    } else {
+        cout << endl;
         for (int i = 0; i < tasks.size(); i++) {
             cout << listNumber++ << ". " << tasks[i] << endl;
         }
     }
+}
+
+void editTask() {
+    int listNumber = 1;
+    int selectedEditTask;
+
+    if (tasks.empty()) {
+        cout << "\nNo tasks found\n";
+        cout << "\nPress Enter to return to the main menu\n";
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        cin.get();
+    } else {
+        cout << endl;
+        for (int i = 0; i < tasks.size(); i++) {
+            cout << listNumber++ << ". " << tasks[i] << endl;
+        }
+        cout << "\nSelect a task to edit: \n";
+        cin >> selectedEditTask;
+        cout << "\nYou selected a task - " << tasks[selectedEditTask - 1] << " - to be edited." << endl;
+        cout << "\nEnter new task name: \n";
+        cin >> tasks[selectedEditTask - 1];
+    }
+}
+
+void deleteTask() {
+    int listNumber = 1;
+    int selectedDeleteTask;
+    string deleteTask;
+
+    if (tasks.empty()) {
+        cout << "\nNo tasks found\n";
+        cout << "\nPress Enter to return to the main menu\n";
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        cin.get();
+    } else {
+        cout << endl;
+        for (int i = 0; i < tasks.size(); i++) {
+            cout << listNumber++ << ". " << tasks[i] << endl;
+        }
+        cout << "\nSelect a task to delete: \n";
+        cin >> selectedDeleteTask;
+        do {
+            cout << "\nDo you want to delete " << tasks[selectedDeleteTask - 1] <<"? (y/n): \n";
+            cin >> deleteTask;
+
+            if (deleteTask != "y" && deleteTask != "n") {
+                cout << "\nWrong input. Please try again!\n";
+            }
+        } while (deleteTask != "y" && deleteTask != "n");
+        tasks.erase(tasks.begin() + selectedDeleteTask - 1);
+    }
+}
+
+void run() {
+    int choice;
+
+    do {
+        clearScreen();
+        mainMenu();
+        cout << "\nEnter your choice: \n";
+        cin >> choice;
+
+        switch (choice) {
+            case 1:
+                clearScreen();
+                addTaskHeader();
+                addTask();
+                break;
+            case 2:
+                clearScreen();
+                viewTasksHeader();
+                viewTasks();
+                cout << "\nPress Enter to return to the main menu\n";
+                cin.ignore(numeric_limits<streamsize>::max(), '\n');
+                cin.get();
+                break;
+            case 3:
+                clearScreen();
+                editTasksHeader();
+                editTask();
+                break;
+            case 4:
+                clearScreen();
+                deleteTaskHeader();
+                deleteTask();
+            case 5:
+                cout << "\nShutting down...";
+                break;
+            default:
+                if (cin.fail()) {
+                    cin.clear();
+                    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+                    clearScreen();
+                    mainMenu();
+                    cout << "\nInvalid choice\n";
+                    cout << "\nEnter your choice: \n";
+                    cin >> choice;
+                } else {
+                    clearScreen();
+                    mainMenu();
+                    cout << "\nInvalid choice\n";
+                    cout << "\nEnter your choice: \n";
+                    cin >> choice;
+                }
+        }
+    } while (choice != 5);
 }
